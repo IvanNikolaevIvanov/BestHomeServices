70b7fae91d934dea85fc20ccf5eaaa348ec55205
IvanIvanov <ivanivanovat@gmail.com>
2024-03-12 20:03:26 +0200

    Areas\Identity moved to correct Db class

--- BestHomeServices.Infrastructure/Data/BestHomeServicesDb.cs
+++ BestHomeServices.Infrastructure/Data/BestHomeServicesDb.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace BestHomeServices.Data
+namespace BestHomeServices.Data;
+
+public class BestHomeServicesDb : IdentityDbContext<IdentityUser>
 {
-    public class BestHomeServicesDb : IdentityDbContext
+    public BestHomeServicesDb(DbContextOptions<BestHomeServicesDb> options)
+        : base(options)
     {
-        public BestHomeServicesDb(DbContextOptions<BestHomeServicesDb> options)
-            : base(options)
-        {
-        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
     }
 }
