bdefa5c927eb93da317cfdca88c8b6dae21615be
IvanIvanov <ivanivanovat@gmail.com>
2024-03-12 19:52:47 +0200

    Program.cs cleared

---
+++ BestHomeServices/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,38 @@
+﻿using BestHomeServices.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<BestHomeServicesDb>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = true;
+
+                //TO DO: Remove Some Of The Restrictions
+            })
+                 .AddEntityFrameworkStores<BestHomeServicesDb>();
+
+            return services;
+        }
+    }
+}
